bc7016c6ba797bbfed13f68146ad2e90b876184e
Zhi Li <lizhicq@gmail.com>
2019-04-21 23:16:59 -0400

    new

--- main/LRU.py
+++ main/LRU.py
@@ -3,8 +3,18 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.vector = defaultdict(int)
+        self.cache = defaultdict(int)
 
     def get(self, key: int) -> int:
-        self.vector
+        if key not in self.cache:
+            return -1
+        value = self.cache.pop(key)
+        self.cache[key] = value
+        return value
+
     def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        elif len(self.cache) == self.capacity:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
